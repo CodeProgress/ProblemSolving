9a688ac96c326e3d2b893fa272ad96185eb88476
codeprogress <codeprogress1@gmail.com>
2014-01-20 12:20:18 -0500

    Commit problem statement

--- jetStreamOptimization.py
+++ jetStreamOptimization.py
@@ -1,4 +1,43 @@
-# Jet stream optimization
+# -*- coding: utf-8 -*-
+
+'''
+To save energy, a plane can use jet streams 
+It gets the following input to help plan the flight:
+
+The first line contains only 1 integer, which is the constant energy it takes to 
+fly 1 mile WITHOUT jet streams.
+Every subsequent line contains 3 space-separated integers: 
+    the start mile marker of the jet stream, 
+    the end mile marker of the jet stream, 
+    and lastly an integer denoting the overall energy needed 
+    to fly that jet stream’s distance.
+    
+For instance, the line "5 9 13" means it takes 13 energy units
+to fly the 4 miles between miles 5 and 9.
+
+Note that jet streams can overlap, 
+but the plane cannot be on more than one jet stream at a time, 
+and it cannot fly partial jet streams.
+
+Consider the end mile marker of the farthest jet stream as the endpoint.
+
+Write a program that takes in an input file streams.txt
+to plan out the optimal sequence of jet streams the plane
+should fly on to minimize energy consumption throughout the entire journey. 
+All integers in the input file are non-negative. 
+As output, print out the mininum total energy 
+and a list of tuples denoting the jet streams’ endpoints.
+
+For example, given this input:
+23
+0 5 29
+1 5 6
+2 4 3
+1 2 1
+4 5 1
+the minimum total energy needed to fly all 5 miles is 28 energy units, 
+and the optimal sequence of jet streams is [(1, 2), (2, 4), (4, 5)].
+'''
 
 with open('streams.txt', 'r') as text:
 
