83db8a532a4b82154e15d9b6b59b44344822f3e2
codeprogress <codeprogress1@gmail.com>
2014-01-26 23:35:25 -0500

    Commit problem statement

--- pandigitalFib.py
+++ pandigitalFib.py
@@ -1,4 +1,21 @@
 
+# -*- coding: utf-8 -*-
+"""
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+It turns out that F541, which contains 113 digits, 
+is the first Fibonacci number for which the last nine digits are 1-9 pandigital
+(contain all the digits 1 to 9, but not necessarily in order). 
+And F2749, which contains 575 digits, is the first Fibonacci number 
+for which the first nine digits are 1-9 pandigital.
+
+Given that Fk is the first Fibonacci number for which the first nine digits 
+AND the last nine digits are 1-9 pandigital, find k.
+
+PEuler104
+"""
+
 def are_start_end_pan_dig(start, end, goal = set('123456789')):
     """returns True if start is pandigital and end is pandigital
     start, end:  ints
