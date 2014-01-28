770266533cebc101121178bc64aaae8dec8a57a7
codeprogress <codeprogress1@gmail.com>
2014-01-27 23:37:31 -0500

    Update comments, doc strings

--- pandigitalFib.py
+++ pandigitalFib.py
@@ -17,36 +17,41 @@ PEuler104
 """
 
 def are_start_end_pan_dig(start, end, goal = set('123456789')):
-    """returns True if start is pandigital and end is pandigital
+    """returns True if start is pandigital and end is pandigital, else False
     start, end:  ints
     """
     if set(str(end)) != goal:
         return False
-    #digits = int(math.log(x,10)) + 1
+
     if set(str(start)[:9]) != goal:
         return False
+        
     return True
 
 def fib_start_end_gen():
-    """generates the starting 18 digits and ending 9 digits of the numbers 
+    """generates the first 18 digits and last 9 digits of numbers 
     in the fib sequence and yields tuple (start, end) as ints
     """
+    #keep track of starting 18 digits of fib number
     start, startNext = 0, 1
+    
+    #and last 9 digits of fib number
     end, endNext = 0, 1
+    
     while True: 
         yield (start, end)
-        end, endNext = endNext, (end + endNext) % 10**9
+        end, endNext = endNext, (end + endNext) % 10**9 #leaves only last 9 dig
         start, startNext = startNext, start + startNext
         if startNext > 10**18:
-            start /= 10
-            startNext /= 10
+            start /= 10                                 #removes last dig
+            startNext /= 10                                 #if num > 10**18
         
 def find_fib_pandig():
     """returns the Kth fib number where the first 9 digits and last 9 digits
     are pandigital
     """
     x = fib_start_end_gen()
-    count = 0
+    count = 0               #keep track of Kth fib
     while True:
         start, end = x.next()
         if are_start_end_pan_dig(start, end):
