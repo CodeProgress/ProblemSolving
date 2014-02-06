421cf995628e8b5ec83afba64e84d61b76bb4667
CodeProgress <codeprogress1@gmail.com>
2014-02-05 23:58:55 -0500

    Update NthPrime.java

--- NthPrime.java
+++ NthPrime.java
@@ -1,17 +1,18 @@
 
-
 public class NthPrime {
 		
 	public static void main(String[] args){
 
 		System.out.println(nthPrime(10001));
+		
 	}
 		
 	public static int nthPrime(int n){
-		int count = 0;
-		double log2 = Math.log(2);
-		int limit = (int) Math.ceil(n/(1/(Math.log(n)/log2)));
-		int[] nums = new int[limit + 1];
+		int    count = 0;
+		double log2  = Math.log(2);
+		int    limit = (int) Math.ceil(n/(1/(Math.log(n)/log2)));
+		int[]  nums  = new int[limit + 1];
+		
 		for (int i = 2; i <= limit; i++){
 			if (nums[i] == 0){
 				count += 1;
@@ -22,9 +23,11 @@ public class NthPrime {
 					nums[j] = 1;
 				}
 			}
-		}	
+		}
+		
 		return -1;
 	}
+	
 }
 
 	
