1e3e0e0d0e223767e8718162ca42e13dd7d21808
codeprogress <codeprogress1@gmail.com>
2014-03-29 12:23:06 -0400

    Commit bowlingScore.py

---
+++ bowlingScore.py
@@ -0,0 +1,45 @@
+
+def bowling_score(rollsList):
+    """returns the final score for a complete game
+    rollsList = [int, int, int, ...]
+
+    Tests:
+    >>> rollsList = [10,10,5,4,3,7,10,4,5,10,9,1,10,10,2,3]
+    >>> bowling_score(rollsList)
+    178
+    >>> rollsList2 = [10]*12
+    >>> bowling_score(rollsList2)
+    300
+    """
+
+    index = 0       #keep track of position within list
+    score = 0
+    frame = 0
+
+    while frame < 10:
+        frame += 1
+
+        #stirke
+        if rollsList[index] == 10:
+            score += 10
+            score += rollsList[index + 1]
+            score += rollsList[index + 2]
+            index += 1
+
+        #spare
+        elif rollsList[index] + rollsList[index + 1] == 10:
+            score += 10
+            score += rollsList[index + 2]
+            index += 2
+
+        else:
+            score += rollsList[index]
+            score += rollsList[index + 1]
+            index += 2
+    
+    return score
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
