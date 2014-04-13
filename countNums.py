b043f8665a7affad664c8d5673793f83372cf3bc
codeprogress <codeprogress1@gmail.com>
2014-04-12 21:29:49 -0400

    Update countNums.py
    Add recursive implementation

--- countNums.py
+++ countNums.py
@@ -5,7 +5,6 @@
 #PEuler 164
 
 
-
 def count_nums(numDigits = 20, threeConsecLimit = 9):
     if numDigits < 2:
         return -1
@@ -30,7 +29,39 @@ def count_nums(numDigits = 20, threeConsecLimit = 9):
 
     return sum(firstTwo.values())
 
+def count_nums_rec(numDigits=20, firstTwo=None):
+    """recursive implementation"""
+
+    if firstTwo == None:
+        firstTwo = {(x,y):1 for x in range(1,10) 
+                            for y in range(10) 
+                                if x+y <= 9}
+    #Base Case    
+    if numDigits <= 2:
+        return sum(firstTwo.values())
+    
+    nextTwo = {}
+    for a, b in firstTwo.keys():
+        posThirdVals = range(9 - a - b + 1)
+        for c in posThirdVals:
+            if (b,c) in nextTwo:
+                nextTwo[(b, c)] += firstTwo[(a,b)]
+            else:
+                nextTwo[(b, c)] = firstTwo[(a, b)]
+        
+    return count_nums_rec(numDigits - 1, nextTwo) 
+
+
 assert count_nums(2) == 45
 assert count_nums(3) == 165
 
-print count_nums()
+assert count_nums_rec(2) == 45
+assert count_nums_rec(3) == 165
+
+result     = count_nums()
+result_rec = count_nums_rec()
+
+assert result == result_rec
+
+print result
+
