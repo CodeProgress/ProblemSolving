5a14d61a8d0c4c48477332a05cd3d4208ae8bed4
codeprogress <codeprogress1@gmail.com>
2014-04-22 00:08:40 -0400

    Update countingFractions.py

--- countingFractions.py
+++ countingFractions.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
 #How many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper
-#fractions for d ≤ 12,000?
-import fractions 
+#fractions for d <= 12,000?
 
-total = 0
-for x in range(1, 12000):
-    for y in range(x/3 + 1, x/2+1):
-        if fractions.gcd(x, y) == 1:
-            total += 1
-print total
+import fractions
+
+def count_fractions(lower, upper, n=12000):
+    total = 0
+
+    for x in xrange(1, n):
+        for y in xrange(int(lower*x + 1), int(upper*x + 1)):
+            if fractions.gcd(x, y) == 1:
+                total += 1
+    return total
+
+lower = fractions.Fraction(1,3)
+upper = fractions.Fraction(1,2)
+
+print count_fractions(lower, upper)
