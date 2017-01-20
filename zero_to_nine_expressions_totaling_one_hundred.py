880a0ae0c95cfd57c9389b64cee0341e324af6c1
CodeProgress <codeprogress1@gmail.com>
2017-01-19 21:44:31 -0500

    Update optimize

--- zero_to_nine_expressions_totaling_one_hundred.py
+++ zero_to_nine_expressions_totaling_one_hundred.py
@@ -8,15 +8,13 @@ import Queue
 
 
 def bfs(start, goal, successor_function):
-    count = 0
     visited = set()
     q = Queue.Queue()
     q.put(start)
     while not q.empty():
         cur_path = q.get()
         if eval(cur_path) == goal and cur_path[-1] == "9":
-            if count <100:
-                print cur_path
+            print cur_path
         for add_on in successor_function(cur_path):
             next_val = cur_path + add_on
             if next_val not in visited:
@@ -25,9 +23,10 @@ def bfs(start, goal, successor_function):
     return "Done!"
 
 def next_expression(val):
-    if val[-2:] == "10":
+    next_int = int(val[-1]) + 1
+    if next_int >= 10:
         return ()
-    next_val = str(int(val[-1]) + 1)
+    next_val = str(next_int)
     minus = "-" + next_val
     plus = "+" + next_val
     num = next_val
